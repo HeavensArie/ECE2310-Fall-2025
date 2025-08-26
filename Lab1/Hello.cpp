2374c8d5e49ba97634dd4b1893722b8406b29896
Donna La <20007229+HeavensArie@users.noreply.github.com>
2025-08-26 15:33:02 -0700

    Initial Commit

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/*
+    Prints a messages for our first assignment
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World Again! \n");
+    return 0;
+}
